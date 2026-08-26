dfe50c9f7bfb955c5acfa5082a5c6c91b8a77aae
Asad Amanat Ali <270855559+bc878vu@users.noreply.github.com>
2026-08-26 17:45:37 +0500

    style: load responsive layout overrides

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./layout-overrides.css";
 import { panelRequest } from "./services/firebasePanelStore";
 
 if (typeof window !== "undefined" && !window.__POWERHOUSE_SAFE_FETCH__) {
