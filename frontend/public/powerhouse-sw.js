2113874c9611be3f9f2c314b9c66cc694a4709f0
Asad Amanat Ali <270855559+bc878vu@users.noreply.github.com>
2026-08-26 18:37:16 +0500

    Refresh PowerHouse push service worker for background notifications

--- frontend/public/powerhouse-sw.js
+++ frontend/public/powerhouse-sw.js
@@ -2,7 +2,7 @@ importScripts("/firebase-config.js");
 importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js");
 
-const CACHE_VERSION = "powerhouse-static-v7";
+const CACHE_VERSION = "powerhouse-static-v8";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/favicon.svg", "/icon-192.svg", "/icon-512.svg"];
 const NETWORK_ONLY_HOSTS = [
   "firestore.googleapis.com",
