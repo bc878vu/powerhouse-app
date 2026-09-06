755560a69db93563af5f2778250e57caa36bd200
Asad Amanat Ali <270855559+bc878vu@users.noreply.github.com>
2026-09-07 02:32:58 +0500

    Align task research with dashboard responsive container

--- frontend/src/UserDashboardWithDuty.jsx
+++ frontend/src/UserDashboardWithDuty.jsx
@@ -3,5 +3,5 @@ import UserDashboard from "./UserDashboard";
 import DashboardTaskResearch from "./components/DashboardTaskResearch";
 
 export default function UserDashboardWithDuty() {
-  return <><UserDashboard /><DashboardTaskResearch /></>;
+  return <><UserDashboard /><div className="px-4 pb-12 md:px-8"><div className="mx-auto max-w-[1650px]"><DashboardTaskResearch /></div></div></>;
 }
